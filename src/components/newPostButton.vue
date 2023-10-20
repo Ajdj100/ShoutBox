8bd4bc25e0d3d6325a70f84e28804eacdaa8a0dd
Alistair Campbell <AJCampbell@conestogac.on.ca>
2023-10-20 03:35:48 -0400

    Implemented some stuff for the create post button as well as wthe catregooreis for the sidebar

--- src/components/newPostButton.vue
+++ src/components/newPostButton.vue
@@ -1,3 +1,12 @@
 <template>
-    
+    <div id="wrapper" class="p-5 bg-brand-light border-brand-main border-2 rounded-2xl my-5 grid grid-cols-3 grid-rows-2 gap-3 max-w-5xl">
+        <textarea type="text" placeholder="Shout about something!" rows="5" class="font-brand col-span-3 w-96 focus:outline-none" />
+        <input type="button" value="Shout!" class="resize-none bg-brand-main text-brand-white col-start-3 font-brand p-3 rounded-lg hover:cursor-pointer px-5">
+    </div>
 </template>
+
+<style scoped>
+textarea {
+    resize: none;
+}
+</style>
--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -1,9 +1,10 @@
 <template>
 <div id="wrapper" class="w-40 ml-40 flex flex-col border-r-2 items-center">
-    <a>hello</a>
-    <a>hello</a>
-    <a>hello</a>
-    <a>hello</a>
+    <a>Home</a>
+    <a>Boxes</a>
+    <a>Profile</a>
+    <a>Settings</a>
+    <a>Logout</a>
 
 </div>
 
